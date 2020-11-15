a097041f2db87d7eb9fb87b90ba880be9494a9d5
Seyona <dgbaker93@gmail.com>
2020-11-14 18:15:50 -0600

    Triplets

--- DictAndHashmaps.py
+++ DictAndHashmaps.py
@@ -55,3 +55,37 @@ def sherlockAndAnagrams(s):
         anagrams += buckets[key] * (buckets[key] - 1) // 2
 
     return anagrams
+
+# Problem: Count Triplets
+# https://www.hackerrank.com/challenges/count-triplets-1/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+
+"""
+    Why does this work?
+    - The problem description lets us assume that the list will be sorted from lowest to highest
+    - While iterating we add each value to the dictionary (with the key being the value we are iterating over)
+    - If we hit the scenario where the current value * r exists in the dictionary we know we potentially hit a triple
+    
+    ex: countTriplets([1,2,2,4],2)
+    loop1: 4 gets added to the tripDict
+    loop2: 2 * 2 = 4, Potential triple found, tripDictPairs[2] is created and assigned to the value of tripDict[i*r]
+    loop3: 2* 2 = 4, Potential triple found again, tripDictPairs[2] gets tripDict[i*r] added to it
+    loop4: 1* 2 = 2, tripDictPairs has a key of 2, so we add that key's value to the number of triples
+    
+    If there was another 4 in the passed list we'd have a total of 4 triples instead of 2. 
+    
+"""
+def countTriplets(arr, r):
+    triplets = 0
+
+    tripDict = {}
+    tripDictPairs = {}
+
+    for i in reversed(arr):
+        if i*r in tripDictPairs:
+            triplets += tripDictPairs[i * r]
+        if i*r in tripDict:
+            tripDictPairs[i] = tripDictPairs.get(i, 0) + tripDict[i*r]
+
+        tripDict[i] = tripDict.get(i, 0) + 1
+
+    return triplets
